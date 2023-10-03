d4ecfa8007ed5660bc2d00da99fe5be881a1c7cf
Peter Thompson <peter.thompson92@gmail.com>
2023-10-02 23:25:03 -0300

    Build Docker image inside build script.

--- build.rs
+++ build.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::env;
+use std::process::Command;
+
+fn main() {
+    // Get the cargo directory
+    let cargo_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    // Run docker build
+    let status = Command::new("docker")
+        .arg("build")
+        .arg("-t")
+        .arg("unet")
+        .arg(cargo_dir)
+        .status()
+        .unwrap();
+
+    if !status.success() {
+        panic!("docker build failed");
+    }
+}
